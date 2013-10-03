45bcbcebe095ff90d3b4182c2dc2f1bc0295cd19
Gerry Pass <rgpass@gmail.com>
2013-10-03 14:35:44 -0400

    Set up Heroku pt5

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ function start(route, handle) {
 
 	// Call the http module function createServer which returns
 	// an object with a method listen
-	http.createServer(onRequest).listen(8888);
+	http.createServer(onRequest).listen(process.env.PORT || 8888);
 
 	console.log("Server has started.");
 }
